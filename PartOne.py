cb6ddd4d6fa2afdf9cb5f9153b92eba5198fdd5f
AlyaSE10 <syskoalina17@gmail.com>
2025-06-22 09:18:20 +0100

    Start work in clean repo. Adding pickle file into gitignore. Cloned Github due to problem with big files. Yesterday tried to commit for hours, but apparently as I created pikle files locally before I added to gitignore line about avoding it Push command still tried to to push these files. After long investigation i made a clone and now try to make a first push in it

--- PartOne.py
+++ PartOne.py
@@ -98,7 +98,7 @@ def parse(df, store_path=Path.cwd() / "pickles", out_name="parsed.pickle"):
     """Parses the text of a DataFrame using spaCy, stores the parsed docs as a column and writes 
     the resulting  DataFrame to a pickle file"""
     df["tokens_spacy"] = df["text"].apply(nlp)
-    df.to_pickle("dataframe_parsed.pkl")
+    #df.to_pickle("dataframe_parsed.pkl")
 
 
     #pass
