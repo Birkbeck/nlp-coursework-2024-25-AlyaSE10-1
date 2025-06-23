1ff95e56389c7f00d356c3993ce6f9a421d44491
AlyaSE10 <syskoalina17@gmail.com>
2025-06-23 18:24:03 +0100

    Working execution of task a(i). Checking the code with excel and printing lines using shape method

--- PartTwo.py
+++ PartTwo.py
@@ -2,21 +2,21 @@
 from pathlib import Path
 import pandas as pd
 
+#loading data from csv file
 with open("p2-texts /hansard40000.csv", mode="r",encoding='utf-8') as file:
-    df=pd.read_csv(file,header=1)
-print(df)
+    df=pd.read_csv(file,header=0)
 
-'''def read_speeches(csv_path):
-    csv_path = p2-texts/hansard40000.csv
-    print(Path.cwd())
-    if not file.exists():
-        raise FileNotFoundError(f"File not found")
-    #print(path)
-    df = pd.read_csv(file, header=1)
-    print(df.head())
+#testing further results with local excel dublicate of hansard40000.csv
+#checking number rows with Labour (Co-op) before replacing the value. Number is the same as in Excel
+filtered = df[df["party"] == 'Labour (Co-op)']
+#print(filtered)
+num_rows = filtered.shape[0]
+print(num_rows)
+#replacing value in column "party"
+df["party"] = df["party"].replace({"Labour (Co-op)":"Labour"}) 
+#checking number of rows after replacment - received 0 rows using "Labour (Co-op)". The function is ready to commit
+filtered = df[df["party"] == 'Labour']
+num_rows = filtered.shape[0]
+print(num_rows)
 
-#print(read_speeches(Path))
-
-if __name__ == "__main__":  
-    read_speeches()'''
 
