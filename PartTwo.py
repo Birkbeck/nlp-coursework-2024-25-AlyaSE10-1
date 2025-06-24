1460b6641ced91ad26dff810b13cf4e07a0146d8
AlyaSE10 <syskoalina17@gmail.com>
2025-06-24 16:00:53 +0100

    Temporary commit. I am changing the logic in a(i). I had concerns earlier about Speech value deleting. Now I realised that the tasks in this assignment  is sequential so I can reuse the dataframe and i polished data preporation as it is usuful for future training not as it is in the requirement. I commented my thought and interpretation of each step accordingly

--- PartTwo.py
+++ PartTwo.py
@@ -11,46 +11,40 @@ from sklearn.metrics import classification_report, f1_score
 with open("p2-texts /hansard40000.csv", mode="r",encoding='utf-8') as file:
     df=pd.read_csv(file,header=0)
 
-#a(i)
+#a(i) Data preprocessing
 #testing further results with local Excel dublicate of hansard40000.csv
 #checking number rows with Labour (Co-op) before replacing the value. Number is the same as in Excel
 filtered = df[df["party"] == 'Labour (Co-op)']
 #print(filtered)
 #num_rows = filtered.shape[0]
 #print(num_rows)
-#work with a copy of df so the intial df will stay as it is for next tasks in the assignment
-df_for_a= df.copy()
+
 #rename the ‘Labour (Co-op)’ value in ‘party’ column to ‘Labour’
-df_for_a["party"] = df_for_a["party"].replace({"Labour (Co-op)":"Labour"}) 
+df["party"] = df["party"].replace({"Labour (Co-op)":"Labour"}) 
 #checking number of rows after replacment - received 0 rows using "Labour (Co-op)". The function is ready to commit
 #filtered = df[df["party"] == 'Labour']
 #num_rows = filtered.shape[0]
 #print(num_rows)
 
 #a(ii)
-#Searching four most common party names(verified against test Excel)
-top4_party_names = df_for_a["party"].value_counts().head(4)
+#removing value Speaker before finding the top4. As I am preparing the dataset for further training and "party" column will be my target, value Speaker is not a party name, So it is a missing value. I clean only value not rows
+df["party"] == df["party"].replace({"Speaker": ""})
+#Now when I cleaned not relevant Speaker I am searching for top 4 party names that I will predict on a future steps of the task. By default value_counts will ignore the NA "party" value.  
+df = df["party"].value_counts().head(4)
 #print(top4_party_names)
 
-#remove any rows where the value of the ‘party’ column is not one of the four most common party names(verified against test Excel)
-top4_df = df_for_a[df["party"].isin(top4_party_names.index)]
-num_rows = top4_df.shape[0]
+num_rows = df.shape[0]
 #print(num_rows)
 
-#removing value, rows will stay,  replacing with "" . Number of rows in the top4 because according to the requirement we need to make it after gathering
-# most common leaders. If we would clean Speaker before generating top4 the shape of the matrix would be different because Speaker is one of the leaders ub the current version
-top4_df.loc[top4_df["party"] == "Speaker", "party"] = ""
 
-num_rows = top4_df.shape[0]
-#print(num_rows)
+#I will use column 'speech" for predicting the party name. That is why on this step I remove any rows where the value in the ‘speech_class’ column is not ‘Speech’, so has not relevant data
+df = df[df["speech_class"] == 'Speech']
+print(df)
 
-#remove any rows where the value in the ‘speech_class’ column is not ‘Speech’
-top4_df_no_sc = top4_df[top4_df["speech_class"] != 'Speech']
-#print(top4_df_no_sc)
-
-#remove any rows where the text in the ‘speech’ column is less than 1000 characters long
-final_df = top4_df_no_sc[top4_df_no_sc['speech'].str.len() >=1000]
-print(final_df.shape)
+#For detecting nessesary features I need a long text, there is no nesessary information in small speech. That is why I remove any rows where the text in the ‘speech’ column is less than 1000 characters long
+df_prepared = df[df['speech'].str.len() >=1000]
+#Checking my final dataframe. This is my prepared dataset with information about 4 classes where I have enough information for orediction. Dataset is still not balanced but I get rid off parties with extremely small number of observation 
+print(df_prepared)
 
 #b
 #1.Create a vectorizer using default parameters, except for omitting English stopwords and setting max_features to
