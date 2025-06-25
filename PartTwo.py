e031fab2cbbca190ebb305ff694d2f939b0138a3
AlyaSE10 <syskoalina17@gmail.com>
2025-06-25 11:47:17 +0100

    Working version a(ii) , the datafrane is ready for applying classifiers. Poliched the comments for explaining the logic

--- PartTwo.py
+++ PartTwo.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, f1_score
+import numpy as np
 
 #loading data from csv file.The df is the source for our next tasks
 with open("p2-texts /hansard40000.csv", mode="r",encoding='utf-8') as file:
@@ -27,22 +28,24 @@ df["party"] = df["party"].replace({"Labour (Co-op)":"Labour"})
 
 #a(ii)
 #removing value Speaker before finding the top4. As I am preparing the dataset for further training and "party" column will be my target, value Speaker is not a party name, So it is a missing value. I clean only value not rows
-df["party"] = df["party"].replace("Speaker",' ')
-#print(df.shape)
-filtered = df[df["party"]== "Speaker"]
-print(filtered.shape)
-#Now when I cleaned not relevant Speaker I am searching for top 4 party names that I will predict on a future steps of the task. By default value_counts will ignore the NA "party" value.  
-top4 = df["party"].value_counts(dropna=True).head(4)
-print(top4)
-df_top4 =  df[df["party"].isin(top4)]
-print(df_top4.shape)
 
-#I will use column 'speech" for predicting the party name. That is why on this step I remove any rows where the value in the ‘speech_class’ column is not ‘Speech’, so has not relevant data
-df = df[df["speech_class"] == 'Speech']
-print(df)
+df["party"] = df["party"].replace("Speaker",np.nan)
+#testing whether Speaker value was replaced
+#filtered = df[df["party"]== "Speaker"]
+#print(filtered.shape)
+#Now when I cleaned not relevant Speaker I am searching for top 4 party names that I will predict on a future steps of the task. By default value_counts will ignore the NA "party" value.  
+top4 = df["party"].value_counts(dropna=True).head(4).index
+#checking the leaders, that Speaker is not there. Numbers and Leader are verified against excel#
+#print(top4)
+df_top4 =  df[df["party"].isin(top4)]
+#print("Top4", df_top4.shape)
+
+#I will use column 'speech" for predicting the party name. That is why on this step I remove any rows where the value in the ‘speech_class’ column is not ‘Speech’, so has not relevant data. The number of rows is the same as in previous dataframe because we have speech in all observations
+df_top4 = df_top4[df_top4["speech_class"] == 'Speech']
+#print("Only speech", df_top4.shape)
 
 #For detecting nessesary features I need a long text, there is no nesessary information in small speech. That is why I remove any rows where the text in the ‘speech’ column is less than 1000 characters long
-df_prepared = df[df['speech'].str.len() >=1000]
+df_prepared = df_top4[df_top4['speech'].str.len() >=1000]
 #Checking my final dataframe. This is my prepared dataset with information about 4 classes where I have enough information for orediction. Dataset is still not balanced but I get rid off parties with extremely small number of observation 
 print(df_prepared.shape)
 '''
